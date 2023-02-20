4fdaa32c5700befea786752c0c1118e0edbdef35
Carlos A. Carro Duplá <ccarrodupla@gmail.com>
2023-02-19 22:13:32 -0500

    Download podcast files

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 ## Podcasts Export
 
+Some time go Apple changed its Podcasts app and removed the option of saving aside audio/video files.
+Maybe they will add it back, maybe they will not. This is a small program to save those files aside...
+
 ### Setup
 
-This thing runs on macOS and Ruby. It may work in other systems, it may not. Ruby comes pre-installed in the last versions of Mac or Linux machines. You can verify that it is present by running:
+This thing runs on macOS and Ruby. It may work in other systems, it may not. Ruby comes pre-installed
+in most Mac or Linux machines. You can verify that it is present by running:
 
     ruby -v
 
 It should show the version of Ruby you have, or err if you've none...
 
-Also it uses a few Ruby software libraries (a.k.a. gems). You can install those extra libraries by running the setup.rb program from the Terminal (only for mac OS):
+Also it uses a few Ruby libraries (a.k.a. gems), and a media tagging library. You can install those
+extra libraries by running the `setup.rb` program from the Terminal (only for mac OS):
 
     ./setup.rb
+
+It may install Homebrew if you don't have it.
 
 ### Run it
 
@@ -18,4 +25,32 @@ First, using the Podcasts app, download the Podcast episodes you want to keep. T
 
     ./podcasts_export.rb 
 
-If no arguments are passed, I will list the names of the Podcasts where there are episodes downloaded in the Podcast app.
+It would be always create a folder '~/Documents/Podcasts_Exports' if it doesn't already exist.
+
+If no arguments are passed, I will list the names of the Podcasts where there are episodes downloaded
+in the Podcast app.
+
+#### Options
+
+1st argument: the name of the podcast series to download (run the command without arguments to see the
+list). If the name includes spaces, then quote the name. For example:
+
+    ./podcasts_export.rb 'Nature Podcast'
+
+
+The next arguments are probably not so useful:
+
+
+2nd argument: YES or NO. Whether prefix the name of the file by the episode number (default is YES).
+
+    ./podcasts_export.rb 'Nature Podcast' NO
+
+3rd argument: folder where to save the file(s). If not set, it will use '~/Documents/Podcasts_Exports/'
+
+    ./podcasts_export.rb 'Nature Podcast' YES '~/Documents/temp_folder'
+
+4th argument: The Apple Podcasts database file to be read. This is an internal macOS setting, so it is
+probably not a parameter you need to change...
+
+    ./podcasts_export.rb 'Nature Podcast' NO '~/Documents/temp_folder' my_database.db
+
--- setup.rb
+++ setup.rb
@@ -1,20 +1,49 @@
 #!/usr/bin/env -S ruby --enable=jit
 
 # Installing the Ruby gems needed...
+#   and (perhaps) Homebrew and the taglib brew package.
 
 class InstallDependencies
   class << self
 
     def check_and_install!
       install_needed_gems_maybe
+      special_tag_lib_gem_case 
     end
 
     private
 
-    GEMS_NEEDED = { 'sqlite3' => '1.4.2' }
+    GEMS_NEEDED = { 'mimemagic' => '0.4.3', 'sqlite3' => '1.4.2' }
 
     def install_needed_gems_maybe
       GEMS_NEEDED.each{|n,v| install_it_maybe(n,v)}
+    end
+
+    def special_tag_lib_gem_case
+      if gem_present?('taglib-ruby')
+        puts "You already had the 'taglib-ruby' gem installed."
+      else
+        install_homebrew_maybe
+        if system "brew list taglib &>/dev/null"
+          puts "You already had the 'taglib' Homebrew package installed."
+        else
+          puts "Installing 'taglib' via Homebrew...."
+          system 'brew install taglib'
+        end
+        # If doing just this:
+        #     system 'gem install taglib-ruby -v 1.1.0'
+        #   doesn't work, try (README at https://github.com/robinst/taglib-ruby):
+        #     1. Get the TAGLIB_DIR by doing brew info taglib
+        #.        For example: '/opt/homebrew/Cellar/taglib/1.13'
+        #     2. Then run this:
+        #        system 'TAGLIB_DIR=/opt/homebrew/Cellar/taglib/1.13 gem install taglib-ruby -v 1.1.0'
+        taglib_info = (`brew info taglib`).split("\n")
+        taglib_dir = taglib_info[taglib_info.index('https://taglib.org/') + 1].split.first
+        # Gem install:
+        taglib_dir = 'TAGLIB_DIR=' + taglib_dir unless taglib_dir.empty?
+        puts "Installing the 'taglib-ruby' gem..."
+        system "#{taglib_dir} gem install taglib-ruby -v 1.1.3"
+      end
     end
 
     def install_it_maybe(gem_name, version)
@@ -32,6 +61,19 @@ class InstallDependencies
       true
     rescue Gem::MissingSpecError
       false
+    end
+
+    def install_homebrew_maybe
+      if system 'which -s brew'
+        puts 'Homebrew is already installed'
+        # system 'brew update; brew upgrade; brew cleanup'
+      elsif system 'which -s curl'
+        puts 'Installing HomeBrew...'
+        system 'curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh'
+      else
+        raise "curl is needed to continue, strage you don't seem to have it... "\
+              "Please download it and install it (for example, from https://curl.se/download.html)"
+      end
     end
   end
 end
