defcb03758625a901a6ad75770a6126199eae58f
Carlos A. Carro Duplá <ccarrodupla@gmail.com>
2024-11-16 11:06:23 -0500

    Fixes to the taglib-ruby gem install

--- setup.rb
+++ setup.rb
@@ -5,7 +5,6 @@
 
 class InstallDependencies
   class << self
-
     def check_and_install!
       install_needed_gems_maybe
       special_tag_lib_gem_case 
@@ -21,7 +20,7 @@ class InstallDependencies
 
     def special_tag_lib_gem_case
       if gem_present?('taglib-ruby')
-        puts "You already had the 'taglib-ruby' gem installed."
+        puts "The 'taglib-ruby' gem was already installed."
       else
         install_homebrew_maybe
         if system "brew list taglib &>/dev/null"
@@ -36,19 +35,26 @@ class InstallDependencies
         #     1. Get the TAGLIB_DIR by doing brew info taglib
         #.        For example: '/opt/homebrew/Cellar/taglib/1.13'
         #     2. Then run this:
-        #        system 'TAGLIB_DIR=/opt/homebrew/Cellar/taglib/1.13 gem install taglib-ruby -v 1.1.0'
-        taglib_info = (`brew info taglib`).split("\n")
-        taglib_dir = taglib_info[taglib_info.index('https://taglib.org/') + 1].split.first
-        # Gem install:
-        taglib_dir = 'TAGLIB_DIR=' + taglib_dir unless taglib_dir.empty?
-        puts "Installing the 'taglib-ruby' gem..."
-        system "#{taglib_dir} gem install taglib-ruby -v 1.1.3"
+        #        system 'TAGLIB_DIR=/opt/homebrew/Cellar/taglib/1.13.1 gem install taglib-ruby -v 1.1.3'
+        taglib_base_dir = `brew --cellar taglib`.chomp   # '/opt/homebrew/Cellar/taglib'
+        taglib_version = `taglib-config --version`.chomp # '1.13.1'
+        # taglib-ruby 1.1.3 gem install:
+        taglib_dir = "TAGLIB_DIR=#{taglib_base_dir}/#{taglib_version}" unless blank?(taglib_base_dir) || blank?(taglib_version)
+        puts "Installing the 'taglib-ruby' gem, version 1.1.3..."
+        install_success = system "#{taglib_dir} gem install taglib-ruby -v 1.1.3"
+        unless install_success
+          puts "\nUnable to install the Ruby gem taglib-ruby version 1.1.3\n\n" \
+               "You could try the following:\n" \
+               " 1. Run 'brew info taglib' and find, in its output, the directory where Taglib was installed (probably starting by '/opt/homebrew/')\n" \
+               " 2. If, for example this directory is '/opt/homebrew/Cellar/taglib/1.13.1', then run the following command: \n" \
+               "    TAGLIB_DIR=/opt/homebrew/Cellar/taglib/1.13.1 gem install taglib-ruby -v 1.1.3"
+        end
       end
     end
 
     def install_it_maybe(gem_name, version)
       if gem_present?(gem_name)
-        puts "You already had the '#{gem_name}' gem installed."
+        puts "The '#{gem_name}' gem, version #{version}, was already installed."
       else
         puts "Installing the '#{gem_name}' gem (version #{version}) ..."
         command = "gem install #{gem_name}"
@@ -65,7 +71,7 @@ class InstallDependencies
 
     def install_homebrew_maybe
       if system 'which -s brew'
-        puts 'Homebrew is already installed'
+        puts 'Homebrew was already installed'
         # system 'brew update; brew upgrade; brew cleanup'
       elsif system 'which -s curl'
         puts 'Installing HomeBrew...'
@@ -74,6 +80,10 @@ class InstallDependencies
         raise "curl is needed to continue... it is strange you didn't seem to have it... "\
               "Please download it and install it (for example, from https://curl.se/download.html)"
       end
+    end
+
+    def blank?(str)
+      str.nil? || (str.is_a?(String) && str.empty?)
     end
   end
 end
